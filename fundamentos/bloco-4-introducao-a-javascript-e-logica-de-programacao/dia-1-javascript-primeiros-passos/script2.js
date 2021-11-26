59b97447450bf9a6bb6f0e0e6452a118a5bfc271
Otávio <otaviotardellii@gmail.com>
2021-11-26 19:11:50 -0300

    Realização da questão 5

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script2.js
@@ -0,0 +1,14 @@
+const ang1= 10;
+const ang2= 15;
+const ang3= 20;
+const somAng= ang1 + ang2 + ang3;
+
+if (somAng ===180) {
+    console.log(true)
+}
+else if (somAng !=180) {
+    console.log(false)
+}
+else {
+    console.log(null)
+}
