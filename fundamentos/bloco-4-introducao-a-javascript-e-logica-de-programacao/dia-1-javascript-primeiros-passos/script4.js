d714b5617011f7cbcb139efd7cc7a21b910f284d
Otávio <otaviotardellii@gmail.com>
2021-11-28 11:06:17 -0300

    Realização do exercício 8

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script4.js
@@ -0,0 +1,23 @@
+let nota= 50
+
+if (nota >=90) {
+    console.log('a')
+}
+else if (nota >=80) {
+    console.log('b')
+}
+else if (nota >=70) {
+    console.log('c')
+}
+else if (nota >=60) {
+    console.log('d')
+}
+else if (nota >=50) {
+    console.log('e')
+}
+else if (nota <50) {
+    console.log('f')
+}
+else {
+    console.log('Nota inválida')
+}
