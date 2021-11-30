dda3063ad914448179447c51737043ed25a0de10
Otávio <otaviotardellii@gmail.com>
2021-11-30 15:48:11 -0300

    Realização da questão 1

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script1.js
@@ -0,0 +1,11 @@
+let n= 5;
+let ast= '*';
+let line= '';
+
+for (let index= 0; index < n; index +=1) {
+    line= line + ast;
+}
+for (let index=0; index < n; index +=1){
+    console.log(line);
+}
+
