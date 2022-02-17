17d2d1030ef5ad11f206948f925b72eb6e39276a
Otávio <otaviotardellii@gmail.com>
2022-02-17 10:06:30 -0300

    I made react to create Hello World

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercise-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
+function App() {
+  return (
+    Task('Hello World!')
+  );
+}
+
+export default App;
