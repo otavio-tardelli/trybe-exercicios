db887229b682781fc002e08ced87bbddfab2ff39
otavio-tardelli <otaviotardellii@gmail.com>
2022-06-10 12:54:09 -0300

    Finalização dos exercícios

--- back-end/bloco-22-node/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
+++ back-end/bloco-22-node/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
@@ -20,9 +20,22 @@ app.post('/hello', (req, res) => {
 });
 
 app.post('/greetings', (req, res) => {
-  if (user > 17) {
-
+  const { name, age } = req.body;
+  if (age > 17) {
+    return res.status(200).json({
+      message: `Hello, ${name}!`
+    });
   }
+  return res.status(401).json({
+    message: 'Unauthorized'
+  })
+});
+
+app.put('/users/:name/:age', (req, res) => {
+  const { name, age } = req.params;
+  return res.status(200).json({
+    message: `Seu nome é ${name} e você tem ${age} anos de idade`
+  })
 })
 
 app.listen(3001, () => console.log('ouvindo na porta 3001'));
