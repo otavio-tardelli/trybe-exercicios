dade809a84f1bf02189583573912e339c9e668fe
otavio-tardelli <otaviotardellii@gmail.com>
2022-06-23 22:17:39 -0300

    Modificações

--- back-end/bloco-23-arquitetura/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/index.js
+++ back-end/bloco-23-arquitetura/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const {getByAuthorId, getAll} = require('./models/Books');
+const {getByAuthorId, getAll, getById} = require('./models/Books');
 
 const app = express();
 
@@ -13,6 +13,16 @@ app.get('/books', async (req, res) => {
 
   return res.status(200).json(books);
 });
+
+app.get('/book/:id', async (req, res) => {
+  const {id} = req.params;
+
+  const book = await getById(id);
+
+  if (!book) return res.status(404).json({message: 'livro inválido'});
+
+  return res.status(200).json(book);
+})
 
 const PORT = process.env.PORT || 3000;
 
--- back-end/bloco-23-arquitetura/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/models/Books.js
+++ back-end/bloco-23-arquitetura/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/models/Books.js
@@ -21,4 +21,19 @@ const getByAuthorId = async () => {
   }));
 }
 
-module.exports = {getAll, getByAuthorId};
+const getById = async () => {
+  const query = 'SELECT * FROM model_example.books WHERE id=?;';
+
+  const [books] = await connection.execute(query, [id]);
+
+  if (books.length === 0) return null;
+
+
+  return books.map(({id, title, author_id}) => ({
+    id,
+    title, 
+    authorId: author_id,
+  }))[0];
+}
+
+module.exports = {getAll, getByAuthorId, getById};
